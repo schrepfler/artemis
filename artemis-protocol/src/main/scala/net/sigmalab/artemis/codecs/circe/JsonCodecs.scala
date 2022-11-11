5ef8a37403d2d195fdf8eaf94c018894d98cc9fc
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-11-12 00:23:41 +0100

    Update sbt-header to 5.8.0 (#253)
    * Update sbt-header to 5.8.0
    
    * Update file headers with sbt-header 5.8.0
    
    Executed command: sbt headerCreate

--- artemis-protocol/src/main/scala/net/sigmalab/artemis/codecs/circe/JsonCodecs.scala
+++ artemis-protocol/src/main/scala/net/sigmalab/artemis/codecs/circe/JsonCodecs.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Sigmalab
+ * Copyright 2019 Sigmalab
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
